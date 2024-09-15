8c3d7fb1457d7516c3faaf21d179a91c0d608203
Ryann <ryanngohenrui@gmail.com>
2024-09-14 19:02:19 -0700

    Update server

---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+
+app.get("/", (req, res) => res.send("hello!"));
+
+app.get("/stream", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  send(res);
+});
+
+const port = process.env.PORT || 8888;
+const serverName = process.env.SERVER_NAME || "sample";
+
+let i = 0;
+function send (res) {
+    
+    res.write("data: " + `hello from ${serverName} ---- [${i++}]\n\n`);
+
+
+    setTimeout(() => send(res), 1000);
+}
+
+app.listen(port);
+console.log(`Listening on ${port}`)
